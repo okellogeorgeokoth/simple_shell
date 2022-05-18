4b8f157b04251fccd502bacef4b0f655cd74e5ba
George Okello <okoth603@gmail.com>
2022-05-18 09:10:46 +0300

    My commit

--- shell.c
+++ shell.c
@@ -1,89 +1,29 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
- */
-void sig_handler(int sig_num)
-{
-	if (sig_num == SIGINT)
-	{
-		_puts("\n#cisfun$ ");
-	}
-}
-
-/**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
- */
-void _EOF(int len, char *buff)
-{
-	(void)buff;
-	if (len == -1)
-	{
-		if (isatty(STDIN_FILENO))
-		{
-			_puts("\n");
-			free(buff);
-		}
-		exit(0);
-	}
-}
-/**
-  * _isatty - verif if terminal
-  */
-
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
-/**
- * main - Shell
- * Return: 0 on success
+ * main - start shell.
+ *
+ * Return: (0).
  */
 
 int main(void)
 {
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
+	char *row = NULL;
+	char **tokens = NULL;
+	int ct_output = 0;
 
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
+	while (1)
 	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
-		else
-		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
-			{
-				free(buff);
-				f(arv);
-			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
-			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
-			}
-		}
+		if (isatty(STDIN))
+			write(STDOUT, "#holbies$ ", 10);
+
+		row = rd_row();
+		tokens = tokenizer(row);
+		ct_output = Run(tokens, ct_output, row);
+
+		free(row);
+		free(tokens);
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
+
 	return (0);
 }
